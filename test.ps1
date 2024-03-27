30c577fdd4b93a04e6c5981da70c30325ea9e52e
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2024-03-27 21:12:05 +0000

    Demonstrate using argument list

--- test.ps1
+++ test.ps1
@@ -6,6 +6,7 @@ $ErrorActionPreference = 'Continue'
 $WarningPreference = 'Continue'
 $InformationPreference = 'Continue'
 $VerbosePreference = 'Continue'
+$DebugPreference = 'Continue'
 
 trap
 {
@@ -18,18 +19,27 @@ Invoke-Command -ScriptBlock {
 	Write-Output 'write to the output'
 	Write-Error 'write to error' 2>&1
 	Write-Information 'write information'
+	Write-Debug 'write debug'
 	Write-Verbose 'write verbose information to the pipeline with a unique record type in order to identify it and filter'
 	Write-Warning 'this is your final warning'
 } *>&1 | Invoke-PipelineBroadcast -ScriptBlock (
 	{
-		Write-Output > stdout.log
+		param($file)
+		Write-Output > $file
 	},{
-		Where-Object { $_ -is [System.Management.Automation.ErrorRecord] } *> stderr.log
+		param($file)
+		Where-Object { $_ -is [System.Management.Automation.ErrorRecord] } *> $file
 	},{
-		Where-Object { $_ -is [System.Management.Automation.InformationRecord] } *> information.log
+		param($file)
+		Where-Object { $_ -is [System.Management.Automation.InformationRecord] } *> $file
 	},{
-		Where-Object { $_ -is [System.Management.Automation.WarningRecord] } *> warning.log
+		param($file)
+		Where-Object { $_ -is [System.Management.Automation.WarningRecord] } *> $file
 	},{
-		Where-Object { $_ -is [System.Management.Automation.VerboseRecord] } *> verbose.log 
+		param($file)
+		Where-Object { $_ -is [System.Management.Automation.DebugRecord] } *> $file
+	},{
+		param($file)
+		Where-Object { $_ -is [System.Management.Automation.VerboseRecord] } *> $file
 	}
-) -ArgumentList ((0),(0),(0),(0),(0))
+) -ArgumentList ('stdout.log','stderr.log','information.log','warning.log','debug.log','verbose.log')
